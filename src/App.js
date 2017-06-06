63a24380b67614b5d998a31e6f0edd52da3ff5b9
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-06 12:12:11 -0400

    Refactor state variables by using prev state. All in one component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class BaseConverter extends Component {
+  constructor(){
+    super()
+    this.state = {
+      number: 0,
+      binary: 0,
+      octo: 0,
+      hexa: 0
+    }
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log("Enter was pressed")
+      this.setState((prevState, props) => {
+        return {number: document.getElementById("originalNumber").value};
+      })
+      this.setState((prevState, props) => {
+        debugger
+        return {binary: this.baseConvert(prevState.number, 2),
+          octo: this.baseConvert(prevState.number, 8),
+          hexa: this.baseConvert(prevState.number, 16),
+        };
+      })
+    }
+  }
+  baseConvert(input, base) {
+    return (input >>> 0).toString(base);
+  }
 
   render() {
     return (
       <div className="base">
         <label> Base converter </label>
-          <input/>
+          <input id="originalNumber" onKeyPress={this.handleKeyPress}/>
+          <input value={this.state.binary}/>
+          <input value={this.state.octo}/>
+          <input value={this.state.hexa}/>
       </div>
     );
   }
