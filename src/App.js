9fbb836eda97177632bdfab0a4dde8cf6b088eee
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-06 11:07:49 -0400

    Create single component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class BaseConverter extends Component {
+
   render() {
     return (
       <div className="base">
         <label> Base converter </label>
           <input/>
-        <div>
-        <label> Base 2 </label>
-          <input/>
-        </div>
-        <div>
-        <label> Base 8 </label>
-          <input/>
-        </div>
-        <div>
-        <label> Base 16 </label>
-          <input/>
-        </div>
       </div>
     );
   }
